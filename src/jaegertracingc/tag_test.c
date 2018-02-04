9b531e6667becf3bcb234c856e6a90e974f542a2
Isaac Hier <isaachier@gmail.com>
2018-02-03 20:35:50 -0500

    Improve tag coverage

--- src/jaegertracingc/tag_test.c
+++ src/jaegertracingc/tag_test.c
@@ -25,44 +25,47 @@ void test_tag()
     bool result = jaeger_tag_list_init(&tags);
     TEST_ASSERT_TRUE(result);
 
-    jaeger_tag tag = (jaeger_tag) JAEGERTRACING__PROTOBUF__TAG__INIT;
-    tag.key = "test1";
-    tag.value_case = JAEGERTRACING__PROTOBUF__TAG__VALUE_BOOL_VALUE;
-    tag.bool_value = true;
-    result = jaeger_tag_list_append(&tags, &tag);
-    TEST_ASSERT_TRUE(result);
+    for (int i = 0; i < 10; i++) {
+        jaeger_tag tag = (jaeger_tag) JAEGERTRACING__PROTOBUF__TAG__INIT;
+        tag.key = "test1";
+        tag.value_case = JAEGERTRACING__PROTOBUF__TAG__VALUE_BOOL_VALUE;
+        tag.bool_value = true;
+        result = jaeger_tag_list_append(&tags, &tag);
+        TEST_ASSERT_TRUE(result);
 
-    tag.key = "test2";
-    tag.value_case = JAEGERTRACING__PROTOBUF__TAG__VALUE_DOUBLE_VALUE;
-    tag.double_value = 0.12;
-    result = jaeger_tag_list_append(&tags, &tag);
-    TEST_ASSERT_TRUE(result);
+        tag.key = "test2";
+        tag.value_case = JAEGERTRACING__PROTOBUF__TAG__VALUE_DOUBLE_VALUE;
+        tag.double_value = 0.12;
+        result = jaeger_tag_list_append(&tags, &tag);
+        TEST_ASSERT_TRUE(result);
 
-    tag.key = "test3";
-    tag.value_case = JAEGERTRACING__PROTOBUF__TAG__VALUE_LONG_VALUE;
-    tag.long_value = -1234567890;
-    result = jaeger_tag_list_append(&tags, &tag);
-    TEST_ASSERT_TRUE(result);
+        tag.key = "test3";
+        tag.value_case = JAEGERTRACING__PROTOBUF__TAG__VALUE_LONG_VALUE;
+        tag.long_value = -1234567890;
+        result = jaeger_tag_list_append(&tags, &tag);
+        TEST_ASSERT_TRUE(result);
 
-    tag.key = "test4";
-    tag.value_case = JAEGERTRACING__PROTOBUF__TAG__VALUE_STR_VALUE;
-    char buffer[12] = "hello world";
-    tag.str_value = &buffer[0];
-    result = jaeger_tag_list_append(&tags, &tag);
-    TEST_ASSERT_TRUE(result);
-    memset(&buffer, 0, sizeof(buffer));
-    TEST_ASSERT_EQUAL_STRING("hello world", tags.tags[tags.size - 1].str_value);
+        tag.key = "test4";
+        tag.value_case = JAEGERTRACING__PROTOBUF__TAG__VALUE_STR_VALUE;
+        char buffer[12] = "hello world";
+        tag.str_value = &buffer[0];
+        result = jaeger_tag_list_append(&tags, &tag);
+        TEST_ASSERT_TRUE(result);
+        memset(&buffer, 0, sizeof(buffer));
+        TEST_ASSERT_EQUAL_STRING("hello world",
+                                 tags.tags[tags.size - 1].str_value);
 
-    tag.key = "test5";
-    tag.value_case = JAEGERTRACING__PROTOBUF__TAG__VALUE_BINARY_VALUE;
-    char binary_buffer[12] = "hello world";
-    tag.binary_value.data = (uint8_t*) &binary_buffer[0];
-    tag.binary_value.len = sizeof(binary_buffer);
-    result = jaeger_tag_list_append(&tags, &tag);
-    TEST_ASSERT_TRUE(result);
-    memset(&binary_buffer, 0, sizeof(binary_buffer));
-    TEST_ASSERT_EQUAL_STRING(
-        "hello world", (char*) tags.tags[tags.size - 1].binary_value.data);
+        tag.key = "test5";
+        tag.value_case = JAEGERTRACING__PROTOBUF__TAG__VALUE_BINARY_VALUE;
+        char binary_buffer[12] = "hello world";
+        tag.binary_value.data = (uint8_t*) &binary_buffer[0];
+        tag.binary_value.len = sizeof(binary_buffer);
+        result = jaeger_tag_list_append(&tags, &tag);
+        TEST_ASSERT_TRUE(result);
+        memset(&binary_buffer, 0, sizeof(binary_buffer));
+        TEST_ASSERT_EQUAL_STRING(
+            "hello world", (char*) tags.tags[tags.size - 1].binary_value.data);
+    }
 
     jaeger_tag_list_destroy(&tags);
 }
