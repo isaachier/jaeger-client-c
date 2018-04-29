f76a73937cc0c5320c4427a356f1b0376249fee9
Isaac Hier <isaachier@gmail.com>
2018-04-29 18:52:16 -0400

    Begin work on binary codec

--- src/jaegertracingc/propagation.c
+++ src/jaegertracingc/propagation.c
@@ -21,6 +21,8 @@
 
 #include "jaegertracingc/propagation.h"
 
+#include <endian.h>
+
 #include "jaegertracingc/internal/strings.h"
 #include "jaegertracingc/metrics.h"
 #include "jaegertracingc/options.h"
@@ -145,28 +147,32 @@ static inline opentracing_propagation_error_code
 extract_from_text_map_helper(opentracing_text_map_reader* reader,
                              extract_text_map_arg* arg)
 {
+    opentracing_propagation_error_code result =
+        opentracing_propagation_error_code_success;
     arg->ctx = jaeger_malloc(sizeof(*arg->ctx));
     if (arg->ctx == NULL || !jaeger_span_context_init(arg->ctx)) {
-        jaeger_span_context_destroy((jaeger_destructible*) arg->ctx);
-        jaeger_free(arg->ctx);
-        arg->ctx = NULL;
-        return opentracing_propagation_error_code_unknown;
+        result = opentracing_propagation_error_code_unknown;
+        goto cleanup;
     }
-    opentracing_propagation_error_code result =
-        reader->foreach_key(reader, &extract_text_map_callback, arg);
-    if (result == opentracing_propagation_error_code_span_context_corrupted) {
-        arg->metrics->decoding_errors->inc(arg->metrics->decoding_errors, 1);
+    result = reader->foreach_key(reader, &extract_text_map_callback, arg);
+    if (result != opentracing_propagation_error_code_success) {
         goto cleanup;
     }
     if (arg->ctx->trace_id.high == 0 && arg->ctx->trace_id.low == 0 &&
         arg->ctx->debug_id == NULL &&
         jaeger_vector_length(&arg->ctx->baggage) == 0) {
+        /* Successfully decoded an empty span context. */
         result = opentracing_propagation_error_code_success;
         goto cleanup;
     }
     return opentracing_propagation_error_code_success;
 
 cleanup:
+    if (result == opentracing_propagation_error_code_span_context_corrupted &&
+        arg->metrics != NULL) {
+        arg->metrics->decoding_errors->inc(arg->metrics->decoding_errors, 1);
+    }
+    jaeger_span_context_destroy((jaeger_destructible*) arg->ctx);
     jaeger_free(arg->ctx);
     arg->ctx = NULL;
     return result;
@@ -179,6 +185,7 @@ jaeger_extract_from_text_map(opentracing_text_map_reader* reader,
                              const jaeger_headers_config* config)
 {
     assert(ctx != NULL);
+    assert(config != NULL);
     extract_text_map_arg arg = {.ctx = NULL,
                                 .config = config,
                                 .metrics = metrics,
@@ -197,6 +204,7 @@ jaeger_extract_from_http_headers(opentracing_http_headers_reader* reader,
                                  const jaeger_headers_config* config)
 {
     assert(ctx != NULL);
+    assert(config != NULL);
     extract_text_map_arg arg = {.ctx = NULL,
                                 .config = config,
                                 .metrics = metrics,
@@ -209,12 +217,86 @@ jaeger_extract_from_http_headers(opentracing_http_headers_reader* reader,
     return result;
 }
 
+static inline bool read_binary(int (*callback)(void*, char*, size_t),
+                               void* arg,
+                               void* result,
+                               int result_size)
+{
+    char buffer[result_size];
+    const int num_read = callback(arg, buffer, sizeof(buffer));
+    if (num_read != result_size) {
+        return false;
+    }
+    uint64_t value = 0;
+    for (int i = 0; i < num_read; i++) {
+        value |= (buffer[i] << (i * CHAR_BIT));
+    }
+    if (result_size == sizeof(uint64_t)) {
+        *(uint64_t*) result = be64toh(value);
+    }
+    else {
+        assert(result_size == sizeof(uint32_t));
+        *(uint32_t*) result = be32toh(value);
+    }
+    return true;
+}
+
 opentracing_propagation_error_code
 jaeger_extract_from_binary(int (*callback)(void*, char*, size_t),
                            void* arg,
                            jaeger_span_context** ctx,
                            jaeger_metrics* metrics,
-                           const jaeger_headers_config* config);
+                           const jaeger_headers_config* config)
+{
+    assert(callback != NULL);
+    assert(ctx != NULL);
+    assert(config != NULL);
+
+    opentracing_propagation_error_code result =
+        opentracing_propagation_error_code_success;
+    *ctx = (jaeger_span_context*) jaeger_malloc(sizeof(jaeger_span_context));
+    if (*ctx == NULL || !jaeger_span_context_init(*ctx)) {
+        result = opentracing_propagation_error_code_unknown;
+        goto cleanup;
+    }
+
+#define READ_BINARY(member)                                                \
+    do {                                                                   \
+        if (!read_binary(                                                  \
+                callback, arg, &(*ctx)->member, sizeof((*ctx)->member))) { \
+            result =                                                       \
+                opentracing_propagation_error_code_span_context_corrupted; \
+            goto cleanup;                                                  \
+        }                                                                  \
+    } while (0)
+
+    READ_BINARY(trace_id.high);
+    READ_BINARY(trace_id.low);
+    READ_BINARY(span_id);
+    READ_BINARY(parent_id);
+
+#undef READ_BINARY
+
+    char buffer[1];
+    const int num_read = callback(arg, buffer, sizeof(buffer));
+    if (num_read != (int) sizeof(buffer)) {
+        result = opentracing_propagation_error_code_span_context_corrupted;
+        goto cleanup;
+    }
+    (*ctx)->flags = (uint8_t) buffer[0];
+
+    return opentracing_propagation_error_code_success;
+
+cleanup:
+    if (result == opentracing_propagation_error_code_span_context_corrupted &&
+        metrics != NULL) {
+        metrics->decoding_errors->inc(metrics->decoding_errors, 1);
+    }
+    jaeger_span_context_destroy((jaeger_destructible*) *ctx);
+    jaeger_free(*ctx);
+    *ctx = NULL;
+    return result;
+}
 
 opentracing_propagation_error_code
 jaeger_extract_from_custom(opentracing_custom_carrier_reader* reader,
