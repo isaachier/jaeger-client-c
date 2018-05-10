73c90f2e1cea35f155eaf83a195a8ca173e90e36
Isaac Hier <ihier@uber.com>
2018-05-10 14:21:15 -0400

    Add test cases to increase coverage to 100%

--- src/jaegertracingc/siphash_test.c
+++ src/jaegertracingc/siphash_test.c
@@ -26,15 +26,14 @@ void test_siphash()
     } test_cases[] = {{.data = "test", .hash_code = 0x3d5124c4cd58914e},
                       {.data = "12341234", .hash_code = 0x47a9ed4e01555590},
                       {.data = "1", .hash_code = 0x45712a3dbb8d63d2},
-                      {.data = "2", .hash_code = 0x9ee4f4d2aca06b87},
-                      {.data = "3", .hash_code = 0x32f8839d8f6766a5},
-                      {.data = "4", .hash_code = 0x4b08d0a44f1bfb70},
-                      {.data = "5", .hash_code = 0xc8f6efcbad548143},
-                      {.data = "6", .hash_code = 0x1657adc431e01cbd},
-                      {.data = "7", .hash_code = 0x7291fb2a1da09ec9},
-                      {.data = "8", .hash_code = 0x75d748ca73747612},
-                      {.data = "9", .hash_code = 0x88790356b7346f2},
-                      {.data = "10", .hash_code = 0xb85bb114e4d30b40}};
+                      {.data = "10", .hash_code = 0xb85bb114e4d30b40},
+                      {.data = "100", .hash_code = 0x51b74da82050ee7a},
+                      {.data = "1000", .hash_code = 0xc2dcf31e3cea700d},
+                      {.data = "10000", .hash_code = 0x1a858cc904f6b81c},
+                      {.data = "100000", .hash_code = 0x7826d9e168cffab9},
+                      {.data = "1000000", .hash_code = 0x1166ea84c410bf93},
+                      {.data = "10000000", .hash_code = 0xb6a7d994d1d5da57},
+                      {.data = "100000000", .hash_code = 0x161e2914c8c6ec7f}};
     for (size_t i = 0, len = sizeof(test_cases) / sizeof(test_cases[0]);
          i < len;
          i++) {
