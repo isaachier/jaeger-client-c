6fc1ae2c76ac68d9b3c72a4cf83209666aad14c6
Isaac Hier <ihier@uber.com>
2018-05-10 12:58:13 -0400

    Improve siphash test cases

--- src/jaegertracingc/siphash_test.c
+++ src/jaegertracingc/siphash_test.c
@@ -23,12 +23,24 @@ void test_siphash()
     const struct {
         const char* data;
         uint64_t hash_code;
-    } test_cases[] = {{.data = "test", .hash_code = 0x3d5124c4cd58914e}};
+    } test_cases[] = {{.data = "test", .hash_code = 0x3d5124c4cd58914e},
+                      {.data = "12341234", .hash_code = 0x47a9ed4e01555590},
+                      {.data = "1", .hash_code = 0x45712a3dbb8d63d2},
+                      {.data = "2", .hash_code = 0x9ee4f4d2aca06b87},
+                      {.data = "3", .hash_code = 0x32f8839d8f6766a5},
+                      {.data = "4", .hash_code = 0x4b08d0a44f1bfb70},
+                      {.data = "5", .hash_code = 0xc8f6efcbad548143},
+                      {.data = "6", .hash_code = 0x1657adc431e01cbd},
+                      {.data = "7", .hash_code = 0x7291fb2a1da09ec9},
+                      {.data = "8", .hash_code = 0x75d748ca73747612},
+                      {.data = "9", .hash_code = 0x88790356b7346f2},
+                      {.data = "10", .hash_code = 0xb85bb114e4d30b40}};
     for (size_t i = 0, len = sizeof(test_cases) / sizeof(test_cases[0]);
          i < len;
          i++) {
-        TEST_ASSERT_EQUAL_HEX64(
-            test_cases[i].hash_code,
-            siphash(test_cases[i].data, strlen(test_cases[i].data), seed));
+        TEST_ASSERT_EQUAL_HEX64(test_cases[i].hash_code,
+                                siphash((uint8_t*) test_cases[i].data,
+                                        strlen(test_cases[i].data),
+                                        seed));
     }
 }
