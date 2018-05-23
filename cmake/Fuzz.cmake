630f743c9ea6fafe14da5c3dbfac41414da5f23e
Isaac Hier <isaachier@gmail.com>
2018-05-23 06:23:00 -0400

    Implement fuzz testing for siphash

---
+++ cmake/Fuzz.cmake
@@ -0,0 +1,25 @@
+if(__FUZZ)
+  return()
+endif()
+set(__FUZZ ON)
+
+set(_fuzz_flags "-fsanitize=fuzz,address,undefined")
+
+function(append_fuzz_flags var)
+  list(APPEND ${var} ${_fuzz_flags})
+  set(${var} "${${var}}" PARENT_SCOPE)
+endfunction()
+
+function(check_fuzz_support var)
+  set(tmp_dir "${CMAKE_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/CMakeTmp")
+  try_compile(have_fuzz_support
+    "${tmp_dir}/fuzz_test"
+    "${CMAKE_CURRENT_SOURCE_DIR}/cmake/fuzz_test.c"
+    LINK_LIBRARIES ${_fuzz_flags})
+  if(have_fuzz_support)
+    set(${var} ON PARENT_SCOPE)
+    message(STATUS "Checking for fuzz support - Success")
+  else()
+    message(STATUS "Checking for fuzz support - Failed")
+  endif()
+endfunction()
---
+++ cmake/fuzz_test.c
@@ -0,0 +1,9 @@
+#include <stdint.h>
+#include <stdlib.h>
+
+int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size)
+{
+    (void) data;
+    (void) size;
+    return 0;
+}
