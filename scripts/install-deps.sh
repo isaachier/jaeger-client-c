566f36344af8472efdbeb7096c95c34f6e6030f1
Craig Ringer <craig@2ndquadrant.com>
2018-06-12 11:01:53 +0800

    [travis] Only build protobuf and protobuf-c when not using Hunter

--- scripts/install-deps.sh
+++ scripts/install-deps.sh
@@ -50,12 +50,24 @@ function main() {
         bash cmake-3.10.0-rc5-Linux-x86_64.sh --skip-license --prefix="$prefix"
     fi
 
-    if [ ! -f "$prefix/bin/protoc" ]; then
-        build_from_tar "https://github.com/google/protobuf/releases/download/v3.5.1/protobuf-cpp-3.5.1.tar.gz" "$prefix"
-    fi
+    # The protobuf-c library in Trusty is 0.15 and hopeless for our purposes.
+    #
+    # There's a PPA at https://launchpad.net/~maarten-fonville/+archive/ubuntu/protobuf
+    # but at time of writing it didn't have protobuf-c, only protobuf proper. So for now
+    # we must build protocol buffers from source. Only done if Hunter is turned off for
+    # this build, since otherwise we know we can just get it from Hunter.
+    #
+    if [ "${USE_HUNTER:-}" == "off" ]; then
+        if [ ! -f "$prefix/bin/protoc" ]; then
+            build_from_tar "https://github.com/google/protobuf/releases/download/v3.5.1/protobuf-cpp-3.5.1.tar.gz" "$prefix"
+        fi
 
-    if [ ! -f "$prefix/bin/protoc-c" ]; then
-        build_from_tar "https://github.com/protobuf-c/protobuf-c/releases/download/v1.3.0/protobuf-c-1.3.0.tar.gz" "$prefix"
+        # We need a protobuf-c with proto3 support, which is 1.3.0 or later
+        # Try to keep the required version down when possible to suport existing
+        # installs.
+        if [ ! -f "$prefix/bin/protoc-c" ]; then
+            build_from_tar "https://github.com/protobuf-c/protobuf-c/releases/download/v1.3.0/protobuf-c-1.3.0.tar.gz" "$prefix"
+        fi
     fi
 }
 
