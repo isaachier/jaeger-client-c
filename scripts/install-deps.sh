91f0249c77546cb2d34e2cba65c6e3c585f6685b
Craig Ringer <craig@2ndquadrant.com>
2018-06-12 11:01:34 +0800

    [travis] Support building with the locally installed CMake on test host

--- scripts/install-deps.sh
+++ scripts/install-deps.sh
@@ -43,7 +43,7 @@ function main() {
 
     mkdir -p "$prefix"
 
-    if [ ! -f "$prefix/bin/cmake" ]; then
+    if [ ! -f "$prefix/bin/cmake" -a -z "${USE_LOCAL_CMAKE}" ]; then
         if [ ! -f "cmake-3.10.0-rc5-Linux-x86_64.sh" ]; then
             curl -L -O "https://cmake.org/files/v3.10/cmake-3.10.0-rc5-Linux-x86_64.sh"
         fi
